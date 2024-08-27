64c2243ed04e7122571f8989a3302d32cd71fec5
DiegoMD4 <diegolink4@hotmail.com>
2024-08-26 21:14:07 -0600

    Resume downloadable

--- js/constants/text.js
+++ js/constants/text.js
@@ -1,124 +1,128 @@
 export const texts = {
-    en: {
-      header: {
-        about: "About me",
-        projects: "Projects",
-        skills: "Skills",
-        contact: "Contact",
-        themeLight: "Light",
-        themeDark: "Dark",
-        language: "Español"
-      },
-      presentation: {
-        title: "Hi, I'm Diego",
+  en: {
+    header: {
+      about: "About me",
+      projects: "Projects",
+      skills: "Skills",
+      contact: "Contact",
+      themeLight: "Light",
+      themeDark: "Dark",
+      language: "Español"
+    },
+    presentation: {
+      title: "Hi, I'm Diego",
+      description:
+        "As a <strong>Computer Engineer and Full Stack Developer</strong> from 🇭🇳, I thrive on crafting innovative software solutions that push the boundaries of creativity.",
+      available: "Available for work",
+      linkedin: "LinkedIn",
+      email: "Email"
+    },
+    projects: {
+      title: "<i class='fa-solid fa-code'></i> Projects",
+      project1: {
+        title: "Help Desk system",
         description:
-          "As a <strong>Computer Engineer and Full Stack Developer</strong> from 🇭🇳, I thrive on crafting innovative software solutions that push the boundaries of creativity.",
-        available: "Available for work",
-        linkedin: "LinkedIn",
-        email: "Email"
+          "Web system developed to handle user tickets and maintain a record of resolved technical issues. -Note that the code is not for the final product.",
+        code: "Code",
+        demo: "Demo"
       },
-      projects: {
-        title: "<i class='fa-solid fa-code'></i> Projects",
-        project1: {
-          title: "Help Desk system",
-          description:
-            "Web system developed to handle user tickets and maintain a record of resolved technical issues. -Note that the code is not for the final product.",
-          code: "Code",
-          demo: "Demo"
-        },
-        project2: {
-          title: "CEI Website",
-          description:
-            "Website created for the Center for Entrepreneurship and Innovation at CEUTEC. I contributed as a backend developer alongside my classmates in this university project.",
-          code: "Code"
-        }
-      },
-      skills: {
-        title: "<i class='fa-solid fa-user-check'></i> Skills",
-        technicalSkills: {
-          title: "Technical skills",
-          description:
-            "Programming languages, data bases, frameworks I have used"
-        },
-        softSkills: {
-          title: "Soft skills",
-          description: "Aspects that define me as a professional",
-          list: "Patience, perseverance, empathy, responsibility, discipline, self-awareness, passioned, enthusiasm, creativity."
-        }
-      },
-      contact: {
-        title: "<i class='fa-solid fa-address-book'></i> Contact",
-        subtitle: "Let's work together!",
+      project2: {
+        title: "CEI Website",
         description:
-          "If you're looking for a <strong>dedicated developer who is problem-solving oriented and always striving for excellence</strong>, I'm here to assist you!",
-        findMe: "You can find or contact me on:",
-        github: "GitHub",
-        linkedin: "LinkedIn",
-        email: "Email"
-      },
-      footer: {
-        resume: "Take a look to my resume here"
+          "Website created for the Center for Entrepreneurship and Innovation at CEUTEC. I contributed as a backend developer alongside my classmates in this university project.",
+        code: "Code"
       }
     },
-    es: {
-      header: {
-        about: "Sobre mí",
-        projects: "Proyectos",
-        skills: "Habilidades",
-        contact: "Contacto",
-        themeLight: "Claro",
-        themeDark: "Oscuro",
-        language: "English"
-      },
-      presentation: {
-        title: "Hola, soy Diego",
+    skills: {
+      title: "<i class='fa-solid fa-user-check'></i> Skills",
+      technicalSkills: {
+        title: "Technical skills",
         description:
-          "Como <strong>Ingeniero en Computación y Desarrollador Full Stack</strong> de 🇭🇳, me apasiona crear soluciones de software innovadoras que empujen los límites de la creatividad.",
-        available: "Disponible para trabajar",
-        linkedin: "LinkedIn",
-        email: "Correo"
+          "Programming languages, data bases, frameworks I have used"
       },
-      projects: {
-        title: "<i class='fa-solid fa-code'></i> Proyectos",
-        project1: {
-          title: "Sistema de Help Desk",
-          description:
-            "Sistema web desarrollado para gestionar tickets de usuarios y mantener un registro de problemas técnicos resueltos. -Tenga en cuenta que el código no es para el producto final.",
-          code: "Código",
-          demo: "Demo"
-        },
-        project2: {
-          title: "Sitio web de CEI",
-          description:
-            "Sitio web creado para el Centro de Emprendimiento e Innovación en CEUTEC. Contribuí como desarrollador backend junto con mis compañeros en este proyecto universitario.",
-          code: "Código"
-        }
-      },
-      skills: {
-        title: "<i class='fa-solid fa-user-check'></i> Habilidades",
-        technicalSkills: {
-          title: "Habilidades técnicas",
-          description:
-            "Lenguajes de programación, bases de datos, frameworks que he utilizado"
-        },
-        softSkills: {
-          title: "Habilidades blandas",
-          description: "Aspectos que me definen como profesional",
-          list: "Paciencia, perseverancia, empatía, responsabilidad, disciplina, autoconciencia, apasionado, entusiasmo, creatividad."
-        }
-      },
-      contact: {
-        title: "<i class='fa-solid fa-address-book'></i> Contacto",
-        subtitle: "¡Trabajemos juntos!",
-        description:
-          "Si buscas un <strong>desarrollador dedicado, orientado a la resolución de problemas y siempre esforzándose por la excelencia</strong>, ¡estoy aquí para ayudarte!",
-        findMe: "Puedes encontrarme o contactarme en:",
-        github: "GitHub",
-        linkedin: "LinkedIn",
-        email: "Correo"
-      },
-      footer: {
-        resume: "Echa un vistazo a mi currículum aquí"
+      softSkills: {
+        title: "Soft skills",
+        description: "Aspects that define me as a professional",
+        list: "Patience, perseverance, empathy, responsibility, discipline, self-awareness, passioned, enthusiasm, creativity."
       }
+    },
+    contact: {
+      title: "<i class='fa-solid fa-address-book'></i> Contact",
+      subtitle: "Let's work together!",
+      description:
+        "If you're looking for a <strong>dedicated developer who is problem-solving oriented and always striving for excellence</strong>, I'm here to assist you!",
+      findMe: "You can find or contact me on:",
+      github: "GitHub",
+      linkedin: "LinkedIn",
+      email: "Email"
+    },
+    footer: {
+      resume: "Download my resume",
+      href: '/assets/pdf/EN_DiegoMontoya_CV.pdf',
+      download: 'EN_DiegoMontoya_CV'
     }
-  };
+  },
+  es: {
+    header: {
+      about: "Sobre mí",
+      projects: "Proyectos",
+      skills: "Habilidades",
+      contact: "Contacto",
+      themeLight: "Claro",
+      themeDark: "Oscuro",
+      language: "English"
+    },
+    presentation: {
+      title: "Hola, soy Diego",
+      description:
+        "Como <strong>Ingeniero en Computación y Desarrollador Full Stack</strong> de 🇭🇳, me apasiona crear soluciones de software innovadoras que empujen los límites de la creatividad.",
+      available: "Disponible para trabajar",
+      linkedin: "LinkedIn",
+      email: "Correo"
+    },
+    projects: {
+      title: "<i class='fa-solid fa-code'></i> Proyectos",
+      project1: {
+        title: "Sistema de Help Desk",
+        description:
+          "Sistema web desarrollado para gestionar tickets de usuarios y mantener un registro de problemas técnicos resueltos. -Tenga en cuenta que el código no es para el producto final.",
+        code: "Código",
+        demo: "Demo"
+      },
+      project2: {
+        title: "Sitio web de CEI",
+        description:
+          "Sitio web creado para el Centro de Emprendimiento e Innovación en CEUTEC. Contribuí como desarrollador backend junto con mis compañeros en este proyecto universitario.",
+        code: "Código"
+      }
+    },
+    skills: {
+      title: "<i class='fa-solid fa-user-check'></i> Habilidades",
+      technicalSkills: {
+        title: "Habilidades técnicas",
+        description:
+          "Lenguajes de programación, bases de datos, frameworks que he utilizado"
+      },
+      softSkills: {
+        title: "Habilidades blandas",
+        description: "Aspectos que me definen como profesional",
+        list: "Paciencia, perseverancia, empatía, responsabilidad, disciplina, autoconciencia, apasionado, entusiasmo, creatividad."
+      }
+    },
+    contact: {
+      title: "<i class='fa-solid fa-address-book'></i> Contacto",
+      subtitle: "¡Trabajemos juntos!",
+      description:
+        "Si buscas un <strong>desarrollador dedicado, orientado a la resolución de problemas y siempre esforzándose por la excelencia</strong>, ¡estoy aquí para ayudarte!",
+      findMe: "Puedes encontrarme o contactarme en:",
+      github: "GitHub",
+      linkedin: "LinkedIn",
+      email: "Correo"
+    },
+    footer: {
+      resume: "Descarga mi currriculum",
+      href: '/assets/pdf/DiegoMontoya_CV_0.0.pdf',
+      download: 'ES_DiegoMontoya_CV'
+    }
+  }
+};
